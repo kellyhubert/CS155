b94ef0cfd9c9e44dc0f290d8e1e12995b756d9e7
Didier Ishimwe <ishimwed@huskers.unl.edu>
2022-02-21 12:35:27 -0500

    bug fix: practice.c shouldn't have main method

--- practice.c
+++ practice.c
@@ -1,23 +1,25 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-double euclideanDistance(double x1, double y1, double x2, double y2);
-int main(int argc,char **argv){
-if(argc!=5){
-	printf("Usage:%s x1 y1 x2 y2",argv[0]);
-	exit(1);
-}
-double x1,y1,x2,y2;
- x1=atof(argv[1]);
- y1=atof(argv[2]);
- x2=atof(argv[3]);
- y2=atof(argv[4]);
+// #include "practice.h"
+// double euclideanDistance(double x1, double y1, double x2, double y2);
+// int main(int argc,char **argv){
+// if(argc!=5){
+// 	printf("Usage:%s x1 y1 x2 y2",argv[0]);
+// 	exit(1);
+// }
+// double x1,y1,x2,y2;
+//  x1=atof(argv[1]);
+//  y1=atof(argv[2]);
+//  x2=atof(argv[3]);
+//  y2=atof(argv[4]);
+//
+// double dist = euclideanDistance(x1, y1, x2, y2);
+// printf("the distance from(%lf,%lf)to(%lf,%lf) is %lf\n",x1,y1,x2,y2,dist);
+//
+// return 0;
+// }
 
-double dist=euclideanDistance(x1, y1, x2, y2);
-printf("the distance from(%lf,%lf)to(%lf,%lf) is %lf\n",x1,y1,x2,y2,dist);
-
-return 0;
-}
 double euclideanDistance(double x1,double y1,double x2, double y2){
 	return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 }
--- practiceDriver.c
+++ practiceDriver.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<math.h>
 
-#include"practice.h"
+#include "practice.h"
 
 
 int main(int argc,char **argv){
@@ -15,9 +15,8 @@ double y1=atof(argv[2]);
 double x2=atof(argv[3]);
 double y2=atof(argv[4]);
 
-double dist=euclideanDistance(x1, y1, x2, y2);
+double dist = euclideanDistance(x1, y1, x2, y2);
 printf("the distance from(%lf,%lf)to(%lf,%lf) is %lf\n",x1,y1,x2,y2,dist);
 
 return 0;
 }
-
